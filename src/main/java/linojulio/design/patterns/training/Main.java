e7efc62018f6df4954c10e47dde5f93222416b79
Julio Lino <afonsojuliounifesp@gmail.com>
2021-10-25 20:16:48 -0300

    project preparation

--- src/main/java/linojulio/design/patterns/training/Main.java
+++ src/main/java/linojulio/design/patterns/training/Main.java
@@ -1,3 +1,5 @@
+package linojulio.design.patterns.training;
+
 public class Main {
     public static void main(String... args) {
         System.out.println("HEY!");
---
+++ src/test/java/linojulio/design/patterns/training/MainTest.java
@@ -0,0 +1,13 @@
+package linojulio.design.patterns.training;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    public void mainRunTest() {
+        Main.main();
+    }
+
+}
